34f4369dde9db8148f711b709e120fe60077f6ec
Josecoca02 <Josecoca0213@gmail.com>
2022-12-16 10:46:06 +0100

    a
    a

--- examencarlos/main2.js
+++ examencarlos/main2.js
@@ -8,6 +8,4 @@ document.getElementById("saludos").style.visibility="visible";
 let body = document.querySelector("body");
 body.addEventListener("click", ocultarSaludo);
 
-function ocultarSaludo() {
-document.getElementById("saludos").style.visibility="visible";
-}
+
